52d6749a05bed40e45c0a3a8fe27f6d39ddcff08
leducminh1411 <led56955@gmail.com>
2023-07-20 23:18:06 +0700

    tao ham hello trong file main.c

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+void hello ();
+
 int main()
 {
     int a = 3, b = 4;
     int sum;
     
+    hello();
+
     sum = a + b;
     printf ("tong %d và %d la %d",a, b, sum);
 
     return 0;
+}
 
+void hello()
+{
+
+    printf ( "hello\n");
 
 }
